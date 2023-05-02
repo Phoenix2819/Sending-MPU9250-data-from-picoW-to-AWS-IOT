41132d690baa178cb15038d3c4403b02a7629f39
Phoenix2819 <128494997+Phoenix2819@users.noreply.github.com>
2023-05-02 11:29:53 +0530

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,35 @@
+#include <pgmspace.h>
+ 
+#define SECRET
+#define THINGNAME "ur thingname"                         //change this
+ 
+const char WIFI_SSID[] = "SSID";               //change this
+const char WIFI_PASSWORD[] = "password";           //change this
+const char AWS_IOT_ENDPOINT[] = "aws endpoint";       //change this
+ 
+// Amazon Root CA 1
+static const char AWS_CERT_CA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+)EOF";
+ 
+// Device Certificate                                               //change this
+static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+
+ 
+ 
+)KEY";
+ 
+// Device Private Key                                               //change this
+static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
+-----BEGIN RSA PRIVATE KEY-----
+
+-----END RSA PRIVATE KEY-----
+
+ 
+ 
+)KEY";
